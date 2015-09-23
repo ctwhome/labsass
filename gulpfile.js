32eeb99a1c63624e8054c15fa344a6b80e3ece64
unknown <jgarcia@WST-054.lnm.local>
2015-09-23 12:27:43 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
 var gulp = require("gulp");
-var plumber = require('gulp-plumber');
 
-var sass = require("gulp-sass");
-var autoprefix = require("gulp-autoprefixer");
-var sourcemaps = require('gulp-sourcemaps')
-var uncss = require('gulp-uncss');
-var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename');
+var typescript = require('gulp-tsc');
 
 var filter = require('gulp-filter');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var inject = browserSync.stream;
+require('require-dir')('./gulp-tasks');
+
+// var uglify = require('gulp-uglify')
+// var ngAnnotate = require('gulp-ng-annotate')
 
 /**
  * Sources variables
@@ -19,52 +16,35 @@ var inject = browserSync.stream;
 var vars = {
     src: {
         // Sources
-//		    scss: 'sass/styles.scss',
-        scss: 'sass/*.scss',
-        html: './app/**/*.html',
+        proxy: "http://localhost:11124/",                       // App proxy
+        ownServer: "./",
+        scss: [
+            './styles/sass/**/*.scss',
+            './app/**/*.scss'
+        ],
+        scssvendor: "styles/sass/vendor.scss",
+        scsscomponents: "styles/sass/components.scss",
+        components: [                                              // Sass injection
+            './app/**/*.scss',
+            './styles/sass/components/*.scss'
+        ],
+        html: "./**/*.cshtml",
         js: "./app/**/*.js",
-        server: "./app"
     },
     des: {
         // output
-        css: './app/css',
-        js: './app/scripts'
+        js: "./app/scripts"
     }
 };
 
 
 /**
- * Start BrowserSync
+ * Default task with proxy
  */
-gulp.task('browser-sync', function () {
-    browserSync({
-        server: vars.src.server
+gulp.task('default', ['sass', 'sass-vendor', 'sass-components', 'browser-sync'],
+    function () {
+        //gulp.watch("./**/*.js").on('change', browserSync.reload);
+        gulp.watch(vars.src.scss, ['sass']);
+        gulp.watch(vars.src.scssvendor, ['sass-vendor']);
+        gulp.watch(vars.src.components, ['sass-components']);
     });
-});
-
-
-// reloading browsers
-gulp.task('watch', browserSync.reload);
-
-// Injet only css
-gulp.task('inject', function () {
-    return gulp.src('app/styles/*.css')
-        .pipe(browserSync.stream());
-});
-
-
-/**
- * Default task "gulp" linked to the LQ app
- */
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch([
-        'app/**/*.js',
-        'app/**/*.json',
-        'app/**/*.html',
-    ], ['watch']);
-
-    gulp.watch('app/styles/*.css', ['inject']);
-    console.log("\n\n\nRUN WATCH-SASS from the LQ WebApp to update the Styles\n\n\n")
-});
-
-
